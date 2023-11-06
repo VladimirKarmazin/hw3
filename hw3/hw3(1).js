549b36c7785edde45e62de28c3b3d9540e0ac3b4
VladimirKarmazin <128676499+VladimirKarmazin@users.noreply.github.com>
2023-11-06 13:45:50 +0200

    Add files via upload

---
+++ hw3/hw3(1).js
@@ -0,0 +1,7 @@
+let userInput1 = prompt("Введіть перший рядок:")
+let userInput2 = prompt("Введіть другий рядок:");
+let userInput3 = prompt("Введіть третій рядок:");
+
+
+let result = `${userInput1} ${userInput2} ${userInput3}`;
+alert(result);
---
+++ hw3/hw3(2).js
@@ -0,0 +1,8 @@
+let fiveDigitNumber = prompt("Введіть п'ятизначне число:");
+let digit1 = fiveDigitNumber.charAt(0);
+let digit2 = fiveDigitNumber.charAt(1);
+let digit3 = fiveDigitNumber.charAt(2);
+let digit4 = fiveDigitNumber.charAt(3);
+let digit5 = fiveDigitNumber.charAt(4);
+
+alert(`${digit1} ${digit2} ${digit3} ${digit4} ${digit5}`);
