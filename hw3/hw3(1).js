269b80facea24c3fb62947e33dd99dde395c3be5
VladimirKarmazin <128676499+VladimirKarmazin@users.noreply.github.com>
2023-11-06 13:21:29 +0200

    Delete hw3 directory

--- hw3/hw3(1).js
+++
@@ -1,7 +0,0 @@
-let userInput1 = prompt("Введіть перший рядок:")
-let userInput2 = prompt("Введіть другий рядок:");
-let userInput3 = prompt("Введіть третій рядок:");
-
-
-let result = `${userInput1} ${userInput2} ${userInput3}`;
-alert(result);
